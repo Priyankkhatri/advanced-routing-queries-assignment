c2a569e4daf53e6593f16e30083922d4a9f01aaf
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-04-24 01:21:04 +0530

    feat: add get all notes endpoint (GET /api/notes)

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const {
   createNote,
   createBulkNotes,
+  getAllNotes,
 } = require("../controllers/note.controller");
 
 const router = express.Router();
 
 router.post("/bulk", createBulkNotes);
 router.post("/", createNote);
+router.get("/", getAllNotes);
 
 module.exports = router;
