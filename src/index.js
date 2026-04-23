643bc81d3a0a081df81e8ac8780f463541a3b7d3
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-04-24 01:20:37 +0530

    chore: setup project structure

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT);
+  } catch (error) {
+    process.exit(1);
+  }
+};
+
+startServer();
