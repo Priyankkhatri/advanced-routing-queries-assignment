c0983c82123c6ca946d3870fa89d527c75667e60
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-04-24 01:21:05 +0530

    feat: add get note by id endpoint (GET /api/notes/:id)

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const Note = require("../models/note.model");
 
 const createNote = async (req, res) => {
@@ -75,8 +76,43 @@ const getAllNotes = async (req, res) => {
   }
 };
 
+const getNoteById = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid note ID",
+        data: null,
+      });
+    }
+
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+        data: null,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Note fetched successfully",
+      data: note,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: null,
+    });
+  }
+};
+
 module.exports = {
   createNote,
   createBulkNotes,
   getAllNotes,
+  getNoteById,
 };
